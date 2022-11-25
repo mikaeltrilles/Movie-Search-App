a52e041fc0f935a0460404d8a3ca8c665a42426f
Mikael Trilles <11914687-mkaeltrilles@users.noreply.gitlab.com>
2022-11-25 03:29:20 +0100

    🚀 RELEASE: Version Finie et mis en ligne

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Movie-Search-App
 
-#### TP-Javascript - Movie Search App - 2022 [Diginamic](https://www.diginamic.fr/)
+[Visitez le site](https://movie-search-app.mtdevweb.com/)
+
+![wakatime](https://wakatime.com/badge/user/933ebfa6-42e4-4a54-b3fc-658e9f1ab22f/project/48b17fad-daa0-45c5-80c0-69e169e3e74b.svg)
 
 ## 1. Introduction
 
@@ -9,6 +11,14 @@ Ce site permet de rechercher des films et d'afficher les résultats.
 ## 2. Comment est il construit ?
 
 Il est construit en HTML, CSS & JavaScript avec l'utilisation de l'API [TMDB API](https://developers.themoviedb.org/3).
+
+![macOS](https://img.shields.io/badge/mac%20os-000000?style=for-the-badge&logo=apple&logoColor=white)
+![Visual Studio Code](https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white)
+![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)
+
+![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
 
 ## 3. Comment l'utiliser ?
 
@@ -25,3 +35,7 @@ Il est possible de contribuer en proposant des améliorations ou des corrections
 ## 6. License
 
 This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+---
+
+### TP-Javascript - Movie Search App - 2022 [Diginamic](https://www.diginamic.fr/)
--- js/app.js
+++ js/app.js
@@ -2,13 +2,86 @@
 const API_KEY = 'api_key=59463c8e33e49fd47140cadd93773d09'; // API key
 const LANG = '&language=fr-FR';
 const BASE_URL = 'https://api.themoviedb.org/3';  // Base URL pour les requêtes
+const BASE_URL_MOVIE = 'https://www.themoviedb.org/movie/';  // Base URL pour les films
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY + LANG; // URL pour les films populaires
 const IMG_URL = 'https://image.tmdb.org/t/p/w500'; // URL pour les images
 const searchURL = BASE_URL + '/search/movie?' + API_KEY + LANG; // URL pour la recherche
 const genres = [
   {
     "id": 28,
-    "name": "💪 Action"
+    "name": "💪&nbspAction"
+  },
+  {
+    "id": 12,
+    "name": "🌋&nbspAventure"
+  },
+  {
+    "id": 16,
+    "name": "🐵&nbspAnimation"
+  },
+  {
+    "id": 35,
+    "name": "😂&nbspComédie"
+  },
+  {
+    "id": 80,
+    "name": "🔪&nbspCrime"
+  },
+  {
+    "id": 99,
+    "name": "🌍&nbspDocumentaire"
+  },
+  {
+    "id": 18,
+    "name": "🎭&nbspDrame"
+  },
+  {
+    "id": 10751,
+    "name": "👨‍👩‍👦‍👦&nbspFamilial"
+  },
+  {
+    "id": 14,
+    "name": "🦄&nbspFantastique"
+  },
+  {
+    "id": 36,
+    "name": "👑&nbspHistoire"
+  },
+  {
+    "id": 27,
+    "name": "😱&nbspHorreur"
+  },
+  {
+    "id": 10402,
+    "name": "🎸&nbspMusical"
+  },
+  {
+    "id": 9648,
+    "name": "🧙🏻‍♂️&nbspMystère"
+  },
+  {
+    "id": 10749,
+    "name": "💕&nbspRomance"
+  },
+  {
+    "id": 878,
+    "name": "🤖&nbspSciFi"
+  },
+  {
+    "id": 10770,
+    "name": "📺&nbspTéléfilm"
+  },
+  {
+    "id": 53,
+    "name": "🕵🏻‍♂️&nbspThriller"
+  },
+  {
+    "id": 10752,
+    "name": "💣&nbspGuerre"
+  },
+  {
+    "id": 37,
+    "name": "🌵&nbspWestern"
   }
 ]
 
@@ -18,29 +91,35 @@ const search = document.getElementById('search'); // Récupère l'élément sear
 
 getMovies(API_URL); // Appel de la fonction getMovies
 
-function getMovies(url) {
-  fetch(url).then(res => res.json()).then(data => {
-    console.log(data.results)
-    if (data.results.length !== 0) {
-      showMovies(data.results);
-    } else {
-      main.innerHTML = `<h1 class="no-results">Pas de résultat 😢</h1>`
+function getMovies(url) { // Fonction pour récupérer les films
+  fetch(url).then(res => res.json()).then(data => { // Récupère les données
+    console.log(data.results) // Affiche les données dans la console
+    if (data.results.length !== 0) {  // Si les données ne sont pas vides
+      showMovies(data.results); // Appel de la fonction showMovies
+    } else {  // Sinon affiche un message d'erreur
+      main.innerHTML = `<h1 class="no-results">Pas de résultat trouvé 😢</h1>`
     }
-
   })
-
 }
 
 
-function showMovies(data) {
-  main.innerHTML = '';
+function showMovies(data) { // Fonction pour afficher les films
+  main.innerHTML = '';  // Vide le contenu de l'élément main
+  const movieGenres = [];
+  data.forEach(movie => { // Pour chaque film
 
-  data.forEach(movie => {
-    const { original_title, poster_path, vote_average, overview, id } = movie;
-    const movieEl = document.createElement('div');
-    movieEl.classList.add('movie');
+    const { original_title, poster_path, vote_average, overview, id } = movie;  // Récupère les données 
+    //Pour chaque film, je crée un  tableau avec en index l'id du film et en valeur un tableau avec la correespondance entre l'id du genre et le nom du genre grace au tableau genres 
+    movie.genre_ids.forEach(genre => {
+      movieGenres[id] = movieGenres[id] || [];
+      movieGenres[id].push(genres.find(g => g.id === genre).name);
+    });
+    console.log(movieGenres);
+    const movieEl = document.createElement('div');  // Crée un élément div
+    movieEl.classList.add('movie'); //  Ajoute la classe movie à l'élément movieEl
+    // Modifie l'html
     movieEl.innerHTML = `
-          <img src="${poster_path ? IMG_URL + poster_path : "http://via.placeholder.com/1080x1580"}" alt="${original_title}">
+    <img src="${poster_path ? IMG_URL + poster_path : "https://images.unsplash.com/photo-1609743522653-52354461eb27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"}" alt="${original_title}">
 
           <div class="movie-info">
             <h3>${original_title}</h3>
@@ -48,19 +127,20 @@ function showMovies(data) {
           </div>
 
           <div class="overview">
-
-              <h3>Synopsis</h3>
-              ${overview}
-              </div>
-      
-      `
-
+          <!-- Affiche toutes les valeurs du tableau movieGenres en fonction de l'id du film grace au join du tableau -->
+          <div>
+          <h3 class="tagRo">Genres</h3>
+          <p class="tag">${movieGenres[id].join(' • ')}</p>
+          
+          </div>
+          <h3 class="tagRo">Synopsis</h3>
+          <p>${overview.split(' ', 40).join(' ')}...</p>
+              <a class="vigBtn" href='${BASE_URL_MOVIE}${id}${LANG}' target="_blank">Voir fiche</a>
+          </div>
+          `
     main.appendChild(movieEl);
-
-
   })
 }
-
 
 function getColor(vote) {
   if (vote >= 8) {
